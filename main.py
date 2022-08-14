d9481782a2922cc34a6dbb2a69c1bce3194a42ca
Woofer21 <potatoh99@gmail.com>
2022-08-13 23:00:42 -0400

    adds window for translations

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ import keyboard as ky
 import pyautogui as pya
 import pyperclip
 import time
+import tkinter as tk
 
 
 
-ky.add_hotkey("alt + t", lambda: copy())
+ky.add_hotkey("right_alt + t", lambda: translate())
 
-def copy():
+def translate():
     pya.tripleClick(pya.position())
-    time.sleep(1)
+    time.sleep(.1)
 
     ky.press_and_release("ctrl + c")
     time.sleep(.1)
 
     val = pyperclip.paste()
     val = val.replace("\n", "")
-    
-    print(val)
+    window(ts.google(val))
+
+def window(val):
+    window = tk.Tk()
+    value = tk.Label(text=val)
+    value.pack()
+
+    window.wm_attributes("-topmost", 1)
+    window.geometry(f"{len(val)*6}x50+{(pya.position().x-150)}+{(pya.position().y)-150}")
+    window.title("Translation -> EN")
+    window.mainloop()
 
 ky.wait("ctrl + alt + shift + esc")
