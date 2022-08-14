ddc9e28becdd45a9cb8257da97fe1a11cb5dcb86
Woofer21 <potatoh99@gmail.com>
2022-08-13 22:29:56 -0400

    copy system

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import translators as ts
 import keyboard as ky
+import pyautogui as pya
+import pyperclip
+import time
 
 
-def epic():
-    print("epic")
 
-ky.add_hotkey("shift + a", lambda: epic())
+ky.add_hotkey("alt + t", lambda: copy())
 
-ky.wait("alt + shift + esc")
+def copy():
+    pya.tripleClick(pya.position())
+    time.sleep(1)
+
+    ky.press_and_release("ctrl + c")
+    time.sleep(.1)
+
+    val = pyperclip.paste()
+    val = val.replace("\n", "")
+    
+    print(val)
+
+ky.wait("ctrl + alt + shift + esc")
